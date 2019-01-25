510ce0f36ff2e51c40bbdb5acf0314e7149b15f9
Chris Naismith <christophernaismith@gmail.com>
2019-01-25 12:53:41 -0500

    Added Products route, product/s views

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/products', function(request, response) {
+  response.render('products', {});
+});
+
+router.get('/products/:id', function(request, response) {
+  response.render('product', {});
+});
+
 module.exports = router;
